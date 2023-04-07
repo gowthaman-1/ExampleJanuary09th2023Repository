ac86bbd50c243b961ae765ec6cec0e335dd8e742
gowthaman-1 <gowthamansai2721@gmail.com>
2023-04-07 09:06:28 +0530

    Intial Draft

---
+++ DemoProject/src/test/java/com/sgtesting/tests/Users.java
@@ -0,0 +1,25 @@
+package com.sgtesting.tests;
+
+import org.testng.annotations.Test;
+
+public class Users {
+	
+		@Test
+		public void Method1()
+		{
+			System.out.println("Method1 User Successfully");
+		}
+		@Test 
+		public void  Method2() {
+			System.out.println("Method2 User Successfully");
+			
+		}
+		@Test
+		public void Method3() {
+			System.out.println("Method3 User Successfully");
+		}
+		@Test
+		public void Metho4() {
+			System.out.println("Method4 User Successfully");
+		}
+}
